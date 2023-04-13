55cbb7b7fae95a67db8ed278728ffbbc6cdfa589
xiaof520 <810132937@qq.com>
2023-04-13 08:18:46 +0800

    Button异常

--- taro/src/pages/index/index.tsx
+++ taro/src/pages/index/index.tsx
@@ -1,28 +1,37 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { View, Text, Button } from '@tarojs/components'
+import { AtButton, AtForm } from 'taro-ui'
 
 import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './index.scss'
 
 export default class Index extends Component<PropsWithChildren> {
-  componentDidMount () { }
+  componentDidMount() { }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
-  render () {
+  onFinish(values) {
+
+  }
+
+  render() {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+        <AtForm
+          onSubmit={this.onFinish.bind(this)}
+        >
+          <Text>Hello world!</Text>
+          <AtButton type='primary'>I need Taro UI</AtButton>
+          <Text>Taro UI 支持 Vue 了吗？</Text>
+          <AtButton type='primary' circle={true}>支持</AtButton>
+          <Text>共建？</Text>
+          <AtButton type='primary' formType='submit' circle={true}>AtButton</AtButton>
+          <Button type='primary' formType='submit'>Button</Button>
+        </AtForm>
       </View>
     )
   }
